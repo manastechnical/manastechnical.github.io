4bb2c1a454c9c67f708b78d36e487e49aee2f543
Manas <manasjagtap20@gmail.com>
2022-12-25 00:47:13 +0530

    Initial Commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+function Welcome() {
+    return (
+        <div className='w-screen h-screen' >
+            <Spline scene="https://prod.spline.design/MEd67GCDYjwa312g/scene.splinecode" />
+        </div>
+    );
+}
+
+export default Welcome;
