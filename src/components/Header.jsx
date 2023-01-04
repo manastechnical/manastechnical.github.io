522cdf52756d46f661cc6cd14060d5fb774f47fa
Manas <manasjagtap20@gmail.com>
2023-01-04 20:24:57 +0530

    Some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profile from '../assets/profile.png';
+import profile1 from '../assets/profile1.png';
 import circle from '../assets/circle.svg';
 import java from '../assets/java.png';
 import react from '../assets/react.svg';
@@ -22,9 +22,9 @@ const Header = () => {
     }
 
     return (
-        <div className='flex-1 w-full h-full flex-col pt-24 4xl:pt-2 lg:flex-row flex justify-center items-center'>
-            <motion.div className='flex-[0.65] flex flex-col justify-start items-start h-full my-0 mx-8 4xl:w-full 4xl:mr-0 pb-16' whileInView={{ x: [-100, 0], opacity: [0, 1] }} transition={{ duration: 0.5 }}>
-                <div className='w-full flex justify-start items-start lg:justify-end md:items-end flex-col'>
+        <div className='flex-1 w-full h-full flex-col lg:flex-row flex justify-center items-center'>
+            <motion.div className='flex-[0.65] flex flex-col justify-center items-center h-full my-16 lg:my-0 mx-8 4xl:w-full 4xl:mr-0 pb-16' whileInView={{ x: [-100, 0], opacity: [0, 1] }} transition={{ duration: 0.5 }}>
+                <div className='w-full flex justify-center items-center lg:justify-end md:items-end flex-col'>
                     <div className='py-4 px-8 border-white rounded-[15px] flex w-auto shadow-md shadow-gray-500'>
                         <span className='sm:text-5xl text-2xl justify-center items-center'>👋</span>
                         <div style={{ marginLeft: 20 }}>
@@ -40,8 +40,8 @@ const Header = () => {
             </motion.div>
 
             <motion.div className='flex-1 h-full flex justify-end items-end relative' whileInView={{ opacity: [0, 1] }} transition={{ duration: 0.5, delayChildre: 0.5 }}>
-                <img className='w-full h-full object-contain z-1' src={profile} alt="img" />
-                <motion.img whileInView={{ scale: [0, 1] }} transition={{ duration: 1, ease: 'easeInOut' }} className="absolute left-0 right-0 bottom-0 z-0 w-full h-[90%] my-8 mx-0 lg:my-0" src={circle} alt='profile' />
+                <img className='w-full h-full object-contain z-1' src={profile1} alt="img" />
+                <motion.img whileInView={{ scale: [0, 1] }} transition={{ duration: 1, ease: 'easeInOut' }} className="absolute left-0 right-0 bottom-0 z-0 w-full h-[90%] my-8 mx-0 lg:my-0" src={circle} alt='circle' />
             </motion.div>
 
 
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import {BsTwitter,BsInstagram} from 'react-icons/bs';
-import {FaFacebook} from 'react-icons/fa';
+import {FaLinkedin} from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='hidden sm:flex justify-end items-center flex-col p-4'>
-      <div className='w-[40px] h-[40px] rounded-[50%] bg-white my-1 mx-0 border-2 border-solid border-[#e4e4e4] flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-[#313bac] hover:border-[#313bac]'>
+      <a href='https://twitter.com/manas_jagtap20' target="_blank" className='w-[40px] h-[40px] rounded-[50%] bg-white my-1 mx-0 border-2 border-solid border-[#e4e4e4] flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-[#313bac] hover:border-[#313bac]'>
         <BsTwitter className='w-[15px] h-[15px] text-[#6b7688] hover:text-white'/>
-      </div>
-      <div className='w-[40px] h-[40px] rounded-[50%] bg-white my-1 mx-0 border-2 border-solid border-[#e4e4e4] flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-[#313bac] hover:border-[#313bac]'>
+      </a>
+      <a href='https://www.instagram.com/manas_jagtap20/' target="_blank" className='w-[40px] h-[40px] rounded-[50%] bg-white my-1 mx-0 border-2 border-solid border-[#e4e4e4] flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-[#313bac] hover:border-[#313bac]'>
         <BsInstagram className='w-[15px] h-[15px] text-[#6b7688] hover:text-white'/>
-      </div>
-      <div className='w-[40px] h-[40px] rounded-[50%] bg-white my-1 mx-0 border-2 border-solid border-[#e4e4e4] flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-[#313bac] hover:border-[#313bac]'>
-        <FaFacebook className='w-[15px] h-[15px] text-[#6b7688] hover:text-white'/>
-      </div>
+      </a>
+      <a href='https://www.linkedin.com/in/manas-jagtap-1ba90a241/' target="_blank" className='w-[40px] h-[40px] rounded-[50%] bg-white my-1 mx-0 border-2 border-solid border-[#e4e4e4] flex justify-center items-center transition-all duration-300 ease-in-out hover:bg-[#313bac] hover:border-[#313bac]'>
+        <FaLinkedin className='w-[15px] h-[15px] text-[#6b7688] hover:text-white'/>
+      </a>
     </div>
   );
 }
